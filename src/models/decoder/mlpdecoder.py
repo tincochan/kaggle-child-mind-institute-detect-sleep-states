69e6fa7d068a2d4f2d35f99ae06bc2eb034f383b
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2023-12-04 14:35:57 +0800

    Revert "Merge branch 'tubo213:main' into main"
    This reverts commit 964c73eb4a5c2421b6ecfb7b5cce7385460e3cf9, reversing
    changes made to d3171fc6e5305c61c2e124d807a12cfe7321af3e.

--- src/models/decoder/mlpdecoder.py
+++ src/models/decoder/mlpdecoder.py
@@ -1,25 +1,31 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class MLPDecoder(nn.Module):
-    def __init__(self, n_channels: int, hidden_size: int, num_layers: int, n_classes: int):
+    def __init__(self, 
+        n_channels: int,
+        hidden_size: int,
+        num_layers: int,
+        n_classes: int
+    ):
         super(MLPDecoder, self).__init__()
-
+        
         self.num_hidden_layers = num_layers - 1
-
+        
         assert num_layers >= 3
-        modules: list[nn.Module] = []
+        self.mlp = []
         for i in range(num_layers):
-            if i == 0:
-                modules.append(nn.Linear(n_channels, hidden_size))
-                modules.append(nn.GELU())
-            elif i == num_layers - 1:
-                modules.append(nn.Linear(hidden_size, n_classes))
+            if i == 0: 
+                self.mlp.append(nn.Linear(n_channels, hidden_size))
+                self.mlp.append(nn.GELU())
+            elif i == num_layers - 1: 
+                self.mlp.append(nn.Linear(hidden_size, n_classes))
             else:
-                modules.append(nn.Linear(hidden_size, hidden_size))
-                modules.append(nn.GELU())
-        self.mlp = nn.ModuleList(modules)
+                self.mlp.append(nn.Linear(hidden_size, hidden_size))
+                self.mlp.append(nn.GELU())
+        self.mlp = nn.ModuleList(*self.mlp)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass of the model.
